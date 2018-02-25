6775021d102320e8702511c06b10e95854ecd07f
ADubiz [Ayoub D.] <ayoub@sheddapp.com>
2018-02-25 16:17:40 +0400

    Dummy commit

--- src/middlewares.js
+++ src/middlewares.js
@@ -136,6 +136,10 @@ export function handleParseHeaders(req, res, next) {
     delete info.sessionToken;
   }
 
+  if (req.url == "/webhooks/messages") {
+    delete info.sessionToken;
+  }
+
   console.info(JSON.stringify({'headers':req.headers, 'url':req.url, 'body':req.body})); //Track every request we receive
 
   if (!info.sessionToken) {
